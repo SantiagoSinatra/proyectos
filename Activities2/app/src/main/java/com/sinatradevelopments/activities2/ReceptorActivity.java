38dc945db23400113721e7cb18314da9d135d6c8
Unknown <santiagosinatra@gmail.com>
2018-09-19 12:02:49 -0300

    Empece el ejercicio 2

---
+++ Activities2/app/src/main/java/com/sinatradevelopments/activities2/ReceptorActivity.java
@@ -0,0 +1,13 @@
+package com.sinatradevelopments.activities2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReceptorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receptor);
+    }
+}
---
+++ Activities2/app/src/main/java/com/sinatradevelopments/activities2/SenderActivity.java
@@ -0,0 +1,13 @@
+package com.sinatradevelopments.activities2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SenderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sender);
+    }
+}
